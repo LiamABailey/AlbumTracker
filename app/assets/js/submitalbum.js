8d6860853a5f91a32e9db479f5a24662b8d41f48
LiamABailey <bailey.liam102@gmail.com>
2021-12-04 22:01:47 -0500

    Generalized cookie setting, corrected setting of expiry

--- app/assets/js/submitalbum.js
+++ app/assets/js/submitalbum.js
@@ -51,7 +51,8 @@ function setAccessTokens(code, state) {
   request.onreadystatechange=function() {
       if(request.readyState==4) {
         const token_resp = JSON.parse(JSON.parse(request.response));
-        setTokenCookie(token_resp.access_token, token_resp.refresh_token, token_resp.expires_in);
+        setCookie("SpotifyAccessToken",token_resp.access_token, token_resp.expires_in);
+        setCookie("SpotifyRefreshToken", token_resp.refresh_token, null);
       }
   }
   request.send(body);
@@ -69,20 +70,18 @@ function refreshAccessTokens() {
   request.onreadystatechange=function() {
       if(request.readyState==4) {
         const token_resp = JSON.parse(JSON.parse(request.response));
-        setTokenCookie(token_resp.access_token, token_resp.refresh_token, token_resp.expires_in);
+        setCookie("SpotifyAccessToken",token_resp.access_token, token_resp.expires_in);
       }
   }
   request.send(body);
-
 }
 
-
-function setTokenCookie(token, refresh_token, expires_in) {
-  const d =  new Date();
-  // cookie is retained for 24 hours
-  d.setTime(d.getTime() + (expires_in * 1000));
-  let expiry = " expires=" + d.toUTCString() +";";
-  document.cookie = "SpotifyAccessToken="+token +";";
-  document.cookie = expiry;
-  document.cookie = "SpotifyRefreshToken="+refresh_token+";";
+// set a single cookie given name, value, and optional expiration time
+// (in seconds in the future)
+function setCookie(cookie_name, cookie_value, expires_in) {
+  let cookie_str = cookie_name+"="+cookie_value+";";
+  if (expires_in != null) {
+    cookie_str += "max-age=" + expires_in +";";
+  }
+  document.cookie = cookie_str
 }
